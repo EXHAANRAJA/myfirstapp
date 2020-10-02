ba444768aabe198865e1c4bed17328af60e363de
EHSAN UL HAQ <rajaahsanulhaq3@gmail.com>
2020-10-02 15:00:29 +0500

    First Commit

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>I love React Native App</Text>
+      <Text>Great Experience</Text>
       <StatusBar style="auto" />
     </View>
   );
